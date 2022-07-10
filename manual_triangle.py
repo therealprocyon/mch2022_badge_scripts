a5181afdcecc4645d06b2320c0ffb886c53a8ede
TheRealProcyon <cc.oosting@gmail.com>
2022-07-10 14:35:32 +0200

    Update manual_triangle.py

--- manual_triangle.py
+++ manual_triangle.py
@@ -1,6 +1,14 @@
 import display
 import system
 import utime
+import buttons
+
+
+def home_press(pressed):
+    if pressed:
+        system.home()
+            
+buttons.attach(buttons.BTN_HOME, home_press)
 
 display.drawFill(0x000000)
 display.flush()
