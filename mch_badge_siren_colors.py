fd183a420e8b309999056253382df204c0e76d30
TheRealProcyon <cc.oosting@gmail.com>
2022-07-10 14:01:57 +0200

    added home button (not tested)

--- mch_badge_siren_colors.py
+++ mch_badge_siren_colors.py
@@ -1,6 +1,14 @@
 import display
 
 while True:
+    
+        
+    def home_press(pressed):
+        if pressed:
+            system.home()
+            
+    buttons.attach(buttons.BTN_HOME, home_press)
+    
     display.drawFill(0xff0000)
     display.flush()
     time.sleep(1)
