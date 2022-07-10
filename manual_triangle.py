04f4a08047db79df1579eda96938baa8b917ed6b
TheRealProcyon <cc.oosting@gmail.com>
2022-07-10 13:51:07 +0200

    added drawing being shown
    added the drawing of the triangle being shown with time.sleep

--- manual_triangle.py
+++ manual_triangle.py
@@ -1,17 +1,21 @@
 import display
 
+
+#added a feature to show triangle being drawn with time.sleep(0.06)
 display.drawFill(0x000000)
 display.flush()
 
 for i in range(200):
     display.drawPixel(i, i+2, 0xff00ff)
     display.flush()
+    time.sleep(0.06)
     
 x = 200
 
 while x > 1:
     display.drawPixel(x,202,0xff00ff)
     display.flush()
+    time.sleep(0.06)
     x-=1
 
 x=202
@@ -19,6 +23,7 @@ x=202
 while x > 1:
     display.drawPixel(1,x,0xff00ff)
     display.flush()
+    time.sleep(0.06)
     x -= 1
    
 
