ae99c1cef57af3783a1101d23b9da6c5235652a3
TheRealProcyon <cc.oosting@gmail.com>
2022-07-10 13:55:14 +0200

    added returning home to the script :)

--- line.py
+++ line.py
@@ -1,9 +1,12 @@
 import display
-
+import system
 
 display.drawFill(0x000000)
 display.flush()
 for i in range(200):
     display.drawPixel(i, i+2, 0xff00ff)
     display.flush()
-
+    time.sleep(0.06)
+    
+time.sleep(3)
+system.home()
