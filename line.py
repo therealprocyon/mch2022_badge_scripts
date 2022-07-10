06ccd90eb420ebebe6c6edf20522f38e2d2a5860
TheRealProcyon <cc.oosting@gmail.com>
2022-07-10 14:31:40 +0200

    Update line.py

--- line.py
+++ line.py
@@ -1,12 +1,13 @@
 import display
 import system
+import utime
 
 display.drawFill(0x000000)
 display.flush()
 for i in range(200):
     display.drawPixel(i, i+2, 0xff00ff)
     display.flush()
-    time.sleep(0.06)
+    utime.sleep(0.06)
     
-time.sleep(3)
+utime.sleep(3)
 system.home()
