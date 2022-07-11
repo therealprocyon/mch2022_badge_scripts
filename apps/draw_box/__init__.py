e46e10d728f05c32edcc0570ea7a30c775ff5140
TheRealProcyon <cc.oosting@gmail.com>
2022-07-11 12:43:30 +0200

    Create __init__.py

---
+++ apps/draw_box/__init__.py
@@ -0,0 +1,20 @@
+# Code is as-is, it may or may not work as I didn't test the code. 
+# I have added an MD file with output of the console in this directory.
+
+import display,system
+
+display.drawFill(0xaa00aa)
+display.drawRect(30,30,20,30,True,0xaaaa00)
+display.drawLine(60,35,60,60,0xabaa00)
+display.drawLine(50,30,60,35,0xabaa00)
+display.drawLine(60,35,60,60,0xabaa00)
+display.drawLine(60,60,50,60,0xabaa00)
+display.drawLine(30,60,44,64,0xabaa00)
+display.drawLine(44,64,49,64,0xabaa00)
+display.flush()
+display.drawLine(30,60,42,63,0xabaa00)
+display.flush()
+display.drawLine(42,63,55,63,0xabaa00)
+display.flush()
+display.drawLine(55,63,60,60,0xabaa00)
+display.flush()
